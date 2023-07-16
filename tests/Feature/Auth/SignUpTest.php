f11cf8b4fc7b1f7290b9b901a6c514774024d837
Arash Nikbakht <arash.nykbakht@gmail.com>
2023-07-16 23:20:22 +0330

    add test case for signup and run the first test
    these commits are just to show how TDD approach works

--- tests/Feature/Auth/SignUpTest.php
+++ tests/Feature/Auth/SignUpTest.php
@@ -3,28 +3,31 @@
 namespace Tests\Feature\Auth;
 
 use App\Helpers\TableNames;
-use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Testing\Fluent\AssertableJson;
 use Tests\TestCase;
 
 class SignUpTest extends TestCase
 {
 	use RefreshDatabase;
 	
-    /** @test */
+	/** @test */
     public function user_can_be_created(): void
     {
-        $response = $this->post('/api/v1/users/', $this->data());
+        $response = $this->post('/api/v1/auth/signup', $this->data());
 
         $response->assertStatus(200);
+	
+	    $response->assertJson(function (AssertableJson $json) {
+		    $json->has('data');
+	    });
 		
 		$this->assertDatabaseCount(TableNames::user, 1);
 		
-		$user = User::first();
-		
-		$this->assertEquals($this->data()["first_name"], $user->first_name);
+		$this->assertDatabaseHas(TableNames::user, [
+			'national_code' => $this->data()['national_code']
+		]);
     }
 	
 	private function data(): array
@@ -36,7 +39,8 @@ class SignUpTest extends TestCase
 			"national_code" => "1234567890",
 			"email" => "arash.nykbakht@gmail.com",
 			"email_verified_at" => now(),
-			"password" => Hash::make("arash1234"),
+			"password" => "arash1234",
+			"device_name" => ["windows", "firefox", "browser"]
 		];
 	}
 }
